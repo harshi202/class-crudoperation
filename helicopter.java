50858204b1eb7dd6f4fcb46d6dc0e4337598cd97
unknown <harshithxworkz@gmail.com>
2022-01-19 11:14:33 +0530

    using this.name

---
+++ helicopter.java
@@ -0,0 +1,24 @@
+class helicopter{
+	String name;
+	int speed;
+	int capacity;
+	String colour;
+	
+	helicopter(){
+		System.out.println("chopper");
+		
+	}
+	
+	      
+	helicopter(String name,int speed,int capacity,String colour){
+		this ();
+		this.name = name;
+		this.speed = speed;
+		this.capacity = capacity;
+		this.colour = colour;
+		
+	}
+
+	
+	
+}
