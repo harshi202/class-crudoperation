d2e80e4e88d036ad6b154c157608d049a37e6d1a
unknown <harshithxworkz@gmail.com>
2022-01-19 11:07:05 +0530

    crudoperation

---
+++ Details.java
@@ -0,0 +1,16 @@
+class Details {
+	int id;
+	String name;
+	String email;
+	long phnumber;
+	String DOB;
+	
+	Details(int idofstudents,String studentname,String emailid,long phNO,String Dateofbirth){
+		id = idofstudents;
+		name = studentname;
+		email = emailid;
+		phnumber = phNO;
+		DOB = Dateofbirth;
+		
+	}
+}
